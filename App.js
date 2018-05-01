5fc65174b1462ced06fbbc34497e89dba45bc25a
fcpauldiaz <pablo5_diaz@hotmail.com>
2018-04-30 21:31:37 -0600

    wait for fonts to load

--- App.js
+++ App.js
@@ -7,14 +7,24 @@ import configureStore from './src/store/index';
 const { persistor, store } = configureStore();
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
   async componentWillMount() {
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
     });
+    this.setState({ loading: false });
+
   }
   render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
     return <Root store={store} persistor={persistor} />;
   }
 }
