69d59124f9ff1e8886121a4281a506cebfa22bb7
fcpauldiaz <pablo5_diaz@hotmail.com>
2018-05-01 09:46:45 -0600

    fix code style

--- native-base-theme/components/Right.js
+++ native-base-theme/components/Right.js
@@ -1,14 +1,13 @@
-import variable from './../variables/platform';
 
-export default (variables = variable) => {
-	const rightTheme = {
-		'NativeBase.Button': {
-			alignSelf: null,
-		},
-		flex: 1,
-		alignSelf: 'center',
-		alignItems: 'flex-end',
-	};
+export default () => {
+  const rightTheme = {
+    'NativeBase.Button': {
+      alignSelf: null,
+    },
+    flex: 1,
+    alignSelf: 'center',
+    alignItems: 'flex-end',
+  };
 
-	return rightTheme;
+  return rightTheme;
 };
